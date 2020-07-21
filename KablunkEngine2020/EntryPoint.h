101806c40cfd4154d3b17372a4c6a8a4596f5ac0
Sean Browne <brownejamessean@gmail.com>
2020-07-20 18:17:35 -0700

    post build command
    added post build command to copy dll to sandbox

--- KablunkEngine2020/EntryPoint.h
+++ KablunkEngine2020/EntryPoint.h
@@ -10,7 +10,7 @@ int main(int argc, char** argv) {
 
 	KABLUNK_CORE_INFO("Initialized Kablunk Engine");
 	KABLUNK_CORE_WARN("TESTING WARNING SYSTEM");
-	KABLUNK_CORE_ERROR("ERROR");
+	KABLUNK_CLIENT_INFO("CLIENT LOG TEST");
 	kablunk::Application* app = kablunk::CreateApplication();
 	app->Run();
 
--- KablunkEngine2020/Log.h
+++ KablunkEngine2020/Log.h
@@ -1,16 +1,19 @@
 #pragma once
 #include "Core.h"
 
+
+#define LOGURU_THREADNAME_WIDTH 0
+
+
 #include <loguru.hpp>
 #include <loguru.cpp>
 
 
+#define KABLUNK_CORE_ERROR(...) RAW_LOG_F(ERROR, "Kablunk| %s", __VA_ARGS__);
+#define KABLUNK_CORE_WARN(...) RAW_LOG_F(WARNING, "Kablunk| %s", __VA_ARGS__);
+#define KABLUNK_CORE_INFO(...) RAW_LOG_F(INFO, "Kablunk| %s", __VA_ARGS__);
 
-#define KABLUNK_CORE_ERROR(...) RAW_LOG_F(ERROR, "KablunkEngine | %s", __VA_ARGS__);
-#define KABLUNK_CORE_WARN(...) RAW_LOG_F(WARNING, "KablunkEngine | %s", __VA_ARGS__);
-#define KABLUNK_CORE_INFO(...) RAW_LOG_F(INFO, "KablunkEngine | %s", __VA_ARGS__);
-
-#define CLIENT_CORE_ERROR(...) RAW_LOG_F(ERROR, "KablunkEngine | %s", __VA_ARGS__);
-#define CLIENT_CORE_WARN(...) RAW_LOG_F(WARNING, "KablunkEngine | %s", __VA_ARGS__);
-#define CLIENT_CORE_INFO(...) RAW_LOG_F(INFO, "KablunkEngine | %s", __VA_ARGS__);
+#define KABLUNK_CLIENT_ERROR(...) RAW_LOG_F(ERROR, "APP| %s", __VA_ARGS__);
+#define KABLUNK_CLIENT_WARN(...) RAW_LOG_F(WARNING, "APP| %s", __VA_ARGS__);
+#define KABLUNK_CLIENT_INFO(...) RAW_LOG_F(INFO, "APP| %s", __VA_ARGS__);
 
