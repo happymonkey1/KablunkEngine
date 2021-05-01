7160450a3fd9f5426bea745d3cabc183ce615d95
happymonkey1 <brownejamessean@gmail.com>
2021-05-01 04:41:44 -0700

    updated spdlog v1.x branch from remote

--- KablunkEngine2020/src/Kablunk/Core/Log.cpp
+++ KablunkEngine2020/src/Kablunk/Core/Log.cpp
@@ -8,10 +8,10 @@ namespace Kablunk {
 
 	void Log::Init() {
 		spdlog::set_pattern("%^[%T] %n: %v%$");
-		s_CoreLogger = spdlog::stdout_color_mt("Kablunk");
+		s_CoreLogger = spdlog::stdout_color_mt("Kablunk", spdlog::color_mode::automatic); //spdlog::stdout_color_mt("Kablunk");
 		s_CoreLogger->set_level(spdlog::level::trace);
 
-		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_ClientLogger = spdlog::stdout_color_mt("Client", spdlog::color_mode::automatic);
 		s_ClientLogger->set_level(spdlog::level::trace);
 	}
 }
