c8130efa766c75467ce252fad7b7d5911d957249
happymonkey1 <brownejamessean@gmail.com>
2021-07-02 17:17:46 -0700

    work on premake

---
+++ KablunkEngine2020/premake5.lua
@@ -0,0 +1,81 @@
+project "KablunkEngine"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "kablunkpch.h"
+	pchsource "src/kablunkpch.cpp"
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		"vendor/stb_image/**.h",
+		"vendor/stb_image/**.cpp",
+		"vendor/glm/glm/**.hpp",
+		"vendor/glm/glm/**.inl"
+	}
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS",
+		"GLFW_INCLUDE_NONE"
+	}
+
+	includedirs
+	{
+		"src",
+		"vendor/spdlog/include",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.stb_image}",
+		"%{IncludeDir.entt}"
+	}
+
+	links
+	{
+		"GLFW",
+		"Glad",
+		"ImGui",
+		"opengl32.lib"
+	}
+
+	filer "system:windows"
+		systemversion "latest"
+
+		defines 
+		{
+		}
+
+	filter "configurations:Debug"
+		defines "KB_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+		links
+		{
+		}
+	
+	filter "configurations:Release"
+		defines "KB_RELEASE"
+		runtime "Release"
+		symbols "on"
+
+		links
+		{
+		}
+	
+	filter "configurations:Distribution"
+		defines "KB_DISTRIBUTION"
+		runtime "Release"
+		optimize "on"
+
+		links
+		{
+		}
