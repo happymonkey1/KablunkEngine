365adf62c63c712a44db77b520799939618e731c
happymonkey1 <brownejamessean@gmail.com>
2021-08-30 17:50:19 -0700

    added python script to download premake

---
+++ KablunkEngine/src/Kablunk/Utilities/Utilities.cpp
@@ -0,0 +1,11 @@
+#include "kablunkpch.h"
+#include "Kablunk/Utilities/Utilities.h"
+
+namespace Kablunk::Utils
+{
+	template <typename T>
+	void WaitForSecondsCallback(float delay, T callback)
+	{
+		// Add job with callback
+	}
+}
---
+++ KablunkEngine/src/Kablunk/Utilities/Utilities.h
@@ -0,0 +1,10 @@
+#ifndef KABLUNK_UTILITIES_UTILITIES_H
+#define KABLUNK_UTILITIES_UTILITIES_H
+
+namespace Kablunk::Utils
+{
+	template <typename T>
+	static void WaitForSecondsCallback(float delay, T callback);
+}
+
+#endif
