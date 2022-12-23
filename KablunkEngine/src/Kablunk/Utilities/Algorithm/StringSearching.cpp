52740aab58c968a4698206b20f05205bd4c70e7c
happymonkey1 <brownejamessean@gmail.com>
2022-12-22 17:23:27 -0800

    fix compile error

--- KablunkEngine/src/Kablunk/Utilities/Algorithm/StringSearching.cpp
+++ KablunkEngine/src/Kablunk/Utilities/Algorithm/StringSearching.cpp
@@ -6,7 +6,7 @@ namespace Kablunk::util::algorithm::search
 
 std::vector<std::string> rabin_karp_string_match(const std::string_view& search_data, const std::string_view& pattern)
 {
-	auto pattern_hashed =  
+	return {};
 }
 
 } // end namespace Kablunk::util::algorithm::search
