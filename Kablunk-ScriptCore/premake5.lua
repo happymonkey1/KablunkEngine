7e1130bd958ef7e965fe37d8d00670d148aa48be
happymonkey1 <brownejamessean@gmail.com>
2023-11-04 16:12:44 -0700

    update premake config
    - updated Kablunk ScriptCore csharp version to 4.8

--- Kablunk-ScriptCore/premake5.lua
+++ Kablunk-ScriptCore/premake5.lua
@@ -1,6 +1,7 @@
 project "Kablunk-ScriptCore"
 	kind "SharedLib"
 	language "C#"
+	csversion ("4.8")
 
 	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
