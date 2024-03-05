9dd8f52e4616932c2c80eadd471167b8f4e5de9c
happymonkey1 <brownejamessean@gmail.com>
2024-03-05 00:55:06 -0800

    forgot to save

--- KablunkEngine/include/Kablunk/Renderer/renderer_2d_utils.h
+++ KablunkEngine/include/Kablunk/Renderer/renderer_2d_utils.h
@@ -1,10 +1,18 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
 #include <string_view>
 
 namespace kb::render
 {
 
-inline auto compute_text_bounds(std::string_view 
+inline auto compute_text_bounds(
+    std::string_view p_text,
+    u32 p_font_point
+) noexcept -> glm::vec2
+{
+    
+}
 
 }
